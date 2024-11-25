3e358bc7f4423e1f5cda85f08b198efe94e2f3b7
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2024-11-25 17:30:42 +0800

    latest

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// console.log(app.get('env'));
-console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true, // Still valid, ensures parsing the connection string properly.
+    useUnifiedTopology: true // Recommended to handle connection management.
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
+
 console.log('ENVIRONMENT:', process.env.NODE_ENV);
 // START SERVER
 
